e68430a1bf08e67672a13b4ad11935f929e9c7ff
soycolgado <soycolgado@gmail.com>
2020-04-19 04:22:56 -0300

    Finalizada la seccion 12

--- config/config.js
+++ config/config.js
@@ -1,1 +1,2 @@
 module.exports.SEED = 'este-es-un-seed-dificil';
+module.exports.CLIENT_ID = '493686965864-ji46f9lvvrphkquf24ct0nftbuecak99.apps.googleusercontent.com';
--- routes/login.js
+++ routes/login.js
@@ -1,61 +1,167 @@
-var express = require('express');
-var bcrypt = require('bcryptjs');
-var jwt = require('jsonwebtoken');
+var express = require("express");
+var bcrypt = require("bcryptjs");
+var jwt = require("jsonwebtoken");
 
-var SEED = require('../config/config').SEED;
-
+var SEED = require("../config/config").SEED;
+var CLIENT_ID = require("../config/config").CLIENT_ID;
 var app = express();
-var Usuario = require('../models/usuario');
+var Usuario = require("../models/usuario");
 
-app.post('/',(req,res)=>{
+const { OAuth2Client } = require("google-auth-library");
+const client = new OAuth2Client(CLIENT_ID);
 
-    var body = req.body;
+//====================================================
+//INICIO Autenticacion de Google
+//====================================================
+async function verify(token) {
+  const ticket = await client.verifyIdToken({
+    idToken: token,
+    audience: CLIENT_ID, // Specify the CLIENT_ID of the app that accesses the backend
+    // Or, if multiple clients access the backend:
+    //[CLIENT_ID_1, CLIENT_ID_2, CLIENT_ID_3]
+  });
+  const payload = ticket.getPayload();
+  // const userid = payload['sub'];
+  // If request specified a G Suite domain:
+  //const domain = payload['hd'];
+  return {
+    nombre: payload.name,
+    email: payload.email,
+    img: payload.picture,
+    google: true,
+  };
+}
+verify().catch(console.error);
 
-    Usuario.findOne({email: body.email}, (err, usuarioBD)=>{
-        if(err){
-            return res.status(500).json({
-                ok: false,
-                mensaje: 'Error al buscar Usuarios',
-                error: err
-            });
-        }
+app.post("/google", async (req, res) => {
+  var token = req.body.token;
 
-        if(!usuarioBD){
-            return res.status(400).json({
-                ok: false,
-                mensaje: 'Credenciales incorrectas - email',
-                error: {message: ''}
-            });
-        }
+  var googleUser = await verify(token).catch((e) => {
+    return res.status(403).json({
+      ok: false,
+      mensaje: "Token no valido",
+    });
+  });
 
-        if(!bcrypt.compareSync(body.password, usuarioBD.password)){
-                return res.status(400).json({
-                    ok: false,
-                    mensaje: 'Credenciales incorrectas - password',
-                    error: {message: ''}
-                });
-            
-        }
+  Usuario.findOne({ email: googleUser.email }, (err, usuarioBD) => {
+    if (err) {
+      return res.status(500).json({
+        ok: false,
+        mensaje: "Error al buscar el usuario",
+        error: err,
+      });
+    }
 
+    if (usuarioBD) {
+      if (usuarioBD.google === false) {
+        return res.status(400).json({
+          ok: false,
+          mensaje: "Debe usar su autenticacion normal",
+        });
+      } else {
         //====================================================
         //INICIO Creacion de Token
         //====================================================
-        usuarioBD.password = ':)';
-        var token = jwt.sign({usuario: usuarioBD}, SEED,{expiresIn: 14400}); // Horas
+        var token = jwt.sign({ usuario: usuarioBD }, SEED, {
+          expiresIn: 14400,
+        }); // Horas
         //====================================================
         //FIN Creacion de Token
         //====================================================
 
         res.status(200).json({
+          ok: true,
+          usuario: usuarioBD,
+          token: token,
+          id: usuarioBD._id,
+        });
+      }
+    }else{
+        //El usuario no existe, hay que crearlo
+        var usuario = new Usuario();
+
+        usuario.nombre = googleUser.nombre;
+        usuario.email = googleUser.email;
+        usuario.img = googleUser.img;
+        usuario.google = true;
+        usuario.password = ':)';
+
+        usuario.save((err, usuarioDB)=>{
+            //====================================================
+        //INICIO Creacion de Token
+        //====================================================
+        var token = jwt.sign({ usuario: usuarioBD }, SEED, {
+            expiresIn: 14400,
+          }); // Horas
+          //====================================================
+          //FIN Creacion de Token
+          //====================================================
+  
+          res.status(200).json({
             ok: true,
             usuario: usuarioBD,
             token: token,
-            id: usuarioBD._id
+            id: usuarioBD._id,
+          });
         });
+    }
+  });
 
-    });
-
+  // return res.status(200).json({
+  //        ok: true,
+  //        mensaje: 'OK',
+  //        googleUser: googleUser
+  // });
 });
 
+//====================================================
+//FIN Autenticacion de Google
+//====================================================
+
+app.post("/", (req, res) => {
+  var body = req.body;
+
+  Usuario.findOne({ email: body.email }, (err, usuarioBD) => {
+    if (err) {
+      return res.status(500).json({
+        ok: false,
+        mensaje: "Error al buscar Usuarios",
+        error: err,
+      });
+    }
+
+    if (!usuarioBD) {
+      return res.status(400).json({
+        ok: false,
+        mensaje: "Credenciales incorrectas - email",
+        error: { message: "" },
+      });
+    }
+
+    if (!bcrypt.compareSync(body.password, usuarioBD.password)) {
+      return res.status(400).json({
+        ok: false,
+        mensaje: "Credenciales incorrectas - password",
+        error: { message: "" },
+      });
+    }
+
+    //====================================================
+    //INICIO Creacion de Token
+    //====================================================
+    usuarioBD.password = ":)";
+    var token = jwt.sign({ usuario: usuarioBD }, SEED, { expiresIn: 14400 }); // Horas
+    //====================================================
+    //FIN Creacion de Token
+    //====================================================
+
+    res.status(200).json({
+      ok: true,
+      usuario: usuarioBD,
+      token: token,
+      id: usuarioBD._id,
+    });
+  });
+});
 
 module.exports = app;
